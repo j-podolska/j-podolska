89447cec38819e6ee6b1b6d5f915981f1a2acf53
j-podolska <julia.podolska@student.uj.edu.pl>
2026-03-30 11:13:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
+<script>
 const langToggle = document.getElementById('langToggle');
 const themeToggle = document.getElementById('themeToggle');
+
 let currentLang = 'pl';
 
 langToggle.addEventListener('click', () => {
   currentLang = currentLang === 'pl' ? 'en' : 'pl';
+
   document.querySelectorAll('[data-pl]').forEach(el => {
     el.textContent = el.getAttribute(`data-${currentLang}`);
   });
+
   langToggle.textContent = currentLang.toUpperCase();
 });
 
 themeToggle.addEventListener('click', () => {
   document.body.classList.toggle('dark');
 });
+</script>
+
